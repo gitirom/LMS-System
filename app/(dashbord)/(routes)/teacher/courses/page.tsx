0e6b4da399275414002a9baa629c2dc31e118da9
gitirom <wadiaromdane10@gmail.com>
2024-11-24 11:15:34 +0100

    creating the create course UI

--- app/(dashbord)/(routes)/teacher/courses/page.tsx
+++ app/(dashbord)/(routes)/teacher/courses/page.tsx
@@ -1,8 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const  CoursePage = () => {
     return (
-        <div className="">
-            Courses Page
+        <div className="p-6">
+            <Link href='/teacher/create'>
+                <Button>
+                    New Course
+                </Button>
+            </Link>
         </div>
     )
 }
